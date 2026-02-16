eb979386d616f6befa34ce02962e63a6e4a92299
Egor <qvvonk@gmail.com>
2026-02-16 16:36:24 +0000

    Refactor Russian messages in install_fph.ps1
    Updated messages in Russian for better clarity and consistency.

--- install_fph.ps1
+++ install_fph.ps1
@@ -1,9 +1,9 @@
 #requires -Version 5.1
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
-
+ 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
+ 
 function Write-Info {
     param([string]$Message)
     Write-Host $Message -ForegroundColor Cyan
@@ -49,7 +49,7 @@ function Get-PythonCommand {
             return @(($canditate), @())
         }
     }
-
+ 
     return $null
 }
 
@@ -66,10 +66,10 @@ function Install-Python {
     $url = "https://python.org/ftp/python/3.13.12/$installer"
     $tempPath = Join-Path $env:TEMP $installer
 
-    Write-Info "Ñêà÷èâàåì Python 3.13 èç $url"
+    Write-Info "Скачиваем Python 3.13 из $url"
     Invoke-WebRequest -Uri $url -OutFile $tempPath
-
-    Write-Info "Óñòàíàâëèâàåì Python..."
+    
+    Write-Info "Устанавливаем Python..."
     $args = "/passive InstallAllUsers=0 PrependPath=1"
     Start-Process -FilePath $tempPath -ArgumentList $args -Wait
     Remove-Item $tempPath -Force
@@ -86,31 +86,31 @@ function Install-Git {
         $pattern = "arm64.exe"
     }
     else {
-        Write-ErrMsg "Íå óäàëîñü ñêà÷àòü Git. Íåèçâåñòíàÿ àðõèòåêòóðà $env:PROCESSOR_ARCHITECTURE."
+        Write-ErrMsg "Не удалось скачать Git. Неизвестная архитектура $env:PROCESSOR_ARCHITECTURE."
     }
 
-    $headers = @{
+    $headers = @{ 
         "User-Agent" = "FunPayHub Installer"
-        "Access" = "application/vnd.github+json"
+        "Access" = "application/vnd.github+json" 
     }
 
     $release = Invoke-RestMethod -Uri "https://api.github.com/repos/git-for-windows/git/releases/latest" -TimeoutSec 10 -Headers $headers
     $asset = $release.assets | Where-Object {$_.name -match $pattern -and $_.name -notmatch "Portable" } | Select-Object -First 1
-
+    
     if (-not $asset) {
-        throw "Íå óäàëîñü íàéòè óñòàíîâùèê Git, ïîäõîäÿùèé ïîä âàøó ñèñòåìó :("
+        throw "Не удалось найти установщик Git, подходящий под вашу систему :("
     }
 
-    Write-Info "Íàéäåí ïîäõîäÿùèé ðåëèç!"
+    Write-Info "Найден подходящий релиз!"
     echo $asset
 
     $tempPath = Join-Path $env:TEMP $asset.name
 
-    Write-Info "Çàãðóæàåì Git..."
+    Write-Info "Загружаем Git..."
     $headers = @{"User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"}
     Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $tempPath -Headers $headers -TimeoutSec 10
 
-    Write-Info "Óñòàíàâëèâàåì Git..."
+    Write-Info "Устанавливаем Git..."
     Start-Process -FilePath $tempPath -ArgumentList "/SILENT /NORESTART /NOCANCEL /SP-" -Wait
     Remove-Item $tempPath -Force
     Update-PATH
@@ -119,22 +119,22 @@ function Install-Git {
 
 function Select-InstallDir {
     $selected = $null
-
+ 
     try {
         Add-Type -AssemblyName System.Windows.Forms | Out-Null
         $dialog = New-Object System.Windows.Forms.FolderBrowserDialog
-        $dialog.Description = "Âûáåðèòå ïàïêó, êóäà õîòèòå óñòàíîâèòü FunPay Hub."
+        $dialog.Description = "Выберите папку, куда хотите установить FunPay Hub."
         $dialog.ShowNewFolderButton = $true
         $dialog.SelectedPath = [Environment]::GetFolderPath("Desktop")
         if ($dialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
             $selected = $dialog.SelectedPath
         }
     } catch {}
-
+ 
     if (-not $selected) {
-        throw "Äèðåêòîðèÿ óñòàíîâêè íå âûáðàíà."
+        throw "Директория установки не выбрана."
     }
-
+ 
     return $selected
 }
 
@@ -145,10 +145,10 @@ function Clone-Hub {
         [string]$path
     )
 
-    Write-Info "Çàãðóæàþ FunPay Hub..."
+    Write-Info "Загружаю FunPay Hub..."
     & $git clone "https://github.com/funpayhub/funpayhub" $path
     if ($LASTEXITCODE -ne 0) {
-        throw "Íå óäàëîñü ñêëîíèðîâàòü FunPay Hub."
+        throw "Не удалось склонировать FunPay Hub."
     }
 }
 
@@ -162,88 +162,88 @@ function Bootstrap {
     Push-Location $path
     $python_args = $python_args + @("-m", "venv", ".venv")
 
-    Write-Info "Ñîçäàþ âèðòóàëüíîå îêðóæåíèå..."
+    Write-Info "Создаю виртуальное окружение..."
     & $python $python_args
     $python = ".venv\Scripts\python.exe"
 
     & $python -m pip --version *>$null
     if ($LASTEXITCODE -ne 0) {
-        Write-Info "Óñòàíàâëèâàþ pip..."
+        Write-Info "Устанавливаю pip..."
         & $python -m ensurepip --upgrade
     }
 
     if (-not (Test-Path "requirements.txt")) {
-        Write-WarnMsg "requirements.txt íå íàéäåí. Ïðîïóñêàþ óñòàíîâêó çàâèñèìîñòåé."
+        Write-WarnMsg "requirements.txt не найден. Пропускаю установку зависимостей."
     }
     else {
-        Write-Info "Óñòàíàâëèâàþò çàâèñèìîñòè..."
+        Write-Info "Устанавливают зависимости..."
         & $python -m pip install -r requirements.txt
     }
 
-    Write-Info "Çàïóñêàþ bootstrap..."
+    Write-Info "Запускаю bootstrap..."
     & $python bootstrap.py
     if ($LASTEXITCODE -ne 0) {
-        throw "Íå óäàëîñü âûïîëíèòü bootstrap."
+        throw "Не удалось выполнить bootstrap."
     }
 
-    Write-Info "Íàñòðîèì êà êîíôèã..."
+    Write-Info "Настроим ка конфиг..."
     & $python releases\current\launcher.py --setup-config
     if ($LASTEXITCODE -ne 0) {
-        throw "Íå óäàëîñü âûïîëíèòü ïåðâóè÷íóþ íàñòðîéêó."
+        throw "Не удалось выполнить первуичную настройку."
     }
 
-    Write-Info "Ñîçäàþ ñêðèïò äëÿ çàïóñêà..."
+    Write-Info "Создаю скрипт для запуска..."
     $bat = "cmd.exe /k `".venv\Scripts\python.exe releases\current\launcher.py`""
     $encoding = New-Object System.Text.UTF8Encoding $False
     [System.IO.File]::WriteAllText((Join-Path (Get-Location) "run_funpayhub.bat"), $bat, $encoding)
 }
 
 
-# Ñêðèïò
+# Скрипт
 chcp 65001
 
 $python = Get-PythonCommand
 if (-not $python) {
-    Write-Info "Python íå íàéäåí. Íà÷èíàåì óñòàíîâêó."
+    Write-Info "Python не найден. Начинаем установку."
     Install-Python
     $python = Get-PythonCommand
     if (-not $python) {
-        throw "Ïîñëå óñòàíîâêè Python òàê è íå áûë îáíàðóæåí."
+        throw "После установки Python так и не был обнаружен."
     }
 }
-Write-Info "Python îáíàðóæåí."
+Write-Info "Python обнаружен."
 $python_args = $python[1]
 $python = $python[0]
 
 
 $git = Get-GitCommand
 if (-not $git) {
-    Write-Info "Git íå íàéäåí. Íà÷èíàåì óñòàíîâêó."
+    Write-Info "Git не найден. Начинаем установку."
     Install-Git
     $git = Get-GitCommand
     if (-not $git) {
-        throw "Ïîñëå óñòàíîâêè Git òàê è íå áûë îáíàðóæåí."
+        throw "После установки Git так и не был обнаружен."
     }
 }
-Write-Info "Git îáíàðóæåí."
+Write-Info "Git обнаружен."
 
 
-Write-Info "Âûáåðèòå äèðåêòîðèþ, êóäà õîòèòå óñòàíîâèòü FunPay Hub."
+Write-Info "Выберите директорию, куда хотите установить FunPay Hub."
 $install_dir = Select-InstallDir
 if (-not $install_dir) {
-    throw "Äèðåêòîðèÿ óñòàíîâêè íå áûëà âûáðàíà."
+    throw "Директория установки не была выбрана."
 }
 
-Write-Info "Íà÷èíàåì ïðîöåññ óñòàíîâêè FunPay Hub â $install_dir."
+Write-Info "Начинаем процесс установки FunPay Hub в $install_dir."
 Clone-Hub -git $git -path $install_dir
 Bootstrap -python $python -python_args $python_args -path $install_dir
 
-Write-Info "FunPay Hub óñïåøíî óñòàíîâëåí. Äëÿ çàïóñêà èñïîëüçóéòå ôàéë run_funpayhub.bat."
+Write-Info "FunPay Hub успешно установлен. Для запуска используйте файл run_funpayhub.bat."
 
-$answer = Read-Host "Õîòèòå çàïóñòèòü FunPay Hub ñåé÷àñ? [Y/n]"
-if ($answer -match "^[YyÄä]" -or (-not $answer)) {
+$answer = Read-Host "Хотите запустить FunPay Hub сейчас? [Y/n]"
+if ($answer -match "^[YyДд]" -or (-not $answer)) {
     Start-Process cmd.exe -ArgumentList "/k", "run_funpayhub.bat"
 } else {
-    Write-Info "Íó è áîëüíî íàäî áûëî :("
+    Write-Info "Ну и больно надо было :("
 }
 exit 0
